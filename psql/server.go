c817f24131b1755eeb46ad7b908888721577e9d7
srinidhi.rao1 <srinidhi.rao@whitewillow.com>
2021-12-07 19:31:47 +0530

    Adding webserver

--- psql/server.go
+++ psql/server.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
+	"strconv"
+	"strings"
 
 	_ "github.com/lib/pq"
 )
@@ -20,23 +23,45 @@ type User struct {
 	Password string
 }
 
-func main() {
-
+func call_db(id int) User {
+	fmt.Println(id)
 	// Create DB pool
 	db, err := sql.Open("postgres", DB_DSN)
 	if err != nil {
 		log.Fatal("Failed to open a DB connection: ", err)
 	}
+
 	defer db.Close()
 
-	// Create an empty user and make the sql query (using $1 for the parameter)
 	var myUser User
+
 	userSql := "SELECT id, email, password FROM users WHERE id = $1"
 
-	err = db.QueryRow(userSql, 2).Scan(&myUser.ID, &myUser.Email, &myUser.Password)
+	err = db.QueryRow(userSql, id).Scan(&myUser.ID, &myUser.Email, &myUser.Password)
 	if err != nil {
 		log.Fatal("Failed to execute query: ", err)
 	}
 
 	fmt.Printf("Hi %s, welcome back!\n", myUser.Email)
+	return myUser
+}
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		//fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		num, _ := strconv.Atoi(strings.Trim(r.URL.Path, "/"))
+		fmt.Println(num)
+		fmt.Fprintf(w, "Hello, %q", call_db(num))
+
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+	// Create an empty user and make the sql query (using $1 for the parameter)
+
 }
